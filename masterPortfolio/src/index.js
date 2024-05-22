0c024dec8040bc8eebb9a8e0c0ba3a50d2a7bcd5
srisankarr <sankarasubramanian3012@gmail.com>
2024-05-22 10:43:00 +0530

    Deploy to origin repository

--- masterPortfolio/src/index.js
+++ masterPortfolio/src/index.js
@@ -14,7 +14,7 @@ const engine = new Styletron();
 
 ReactDOM.render(
   <StyletronProvider value={engine}>
-    <BaseProvider theme={LightTheme}>
+    <BaseProvider theme={LightTheme} basename="/portfolio">
       <App />
     </BaseProvider>
   </StyletronProvider>,
